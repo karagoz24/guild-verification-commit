7e8db36f7898e8abe5757b024b465abc7f8420f9
karagoz24 <61806077+karagoz24@users.noreply.github.com>
2025-10-31 11:07:04 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T08:05:44.904Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-10-31T08:06:47.821Z
+Commit: Add multi-language support
